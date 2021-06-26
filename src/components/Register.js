bd724a91eba749f3af5e9e4fb4c6dc2206436942
rajesh.t7982 <rajesh.t7982@gmail.comm>
2021-06-26 11:56:05 +0530

    heading added

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,4 +1,4 @@
-import {useState,useContext} from 'react'
+import { useState, useContext } from 'react'
 import { useForm } from "react-hook-form";
 import axios from 'axios'
 import { useHistory } from 'react-router-dom'
@@ -6,9 +6,9 @@ import { TestContext } from '../contexts/TestContext';
 
 function Register() {
 
-        const result1=useContext(TestContext)
-        console.log("result is ",result1)
-   
+    const result1 = useContext(TestContext)
+    console.log("result is ", result1)
+
     const { register, handleSubmit } = useForm();
     const [file, setFile] = useState(null)
     const history = useHistory();
@@ -16,11 +16,11 @@ function Register() {
     const onFormSubmit = (userObj) => {
 
         //create FormData obj
-        let formData=new FormData();
+        let formData = new FormData();
         //add file(s) to formdata obj
-        formData.append('photo',file,file.name)
+        formData.append('photo', file, file.name)
         //add userObj to formData object
-        formData.append("userObj",JSON.stringify(userObj))
+        formData.append("userObj", JSON.stringify(userObj))
 
         //post req
         axios.post("/user/createuser", formData)
@@ -40,7 +40,7 @@ function Register() {
 
     //to get selected
     const onFileSelect = (e) => {
-        
+
         setFile(e.target.files[0])
     }
 
@@ -48,16 +48,18 @@ function Register() {
 
 
     return (
+        <div>
+            <h1>register here</h1>
+            <form className="w-50 mx-auto mt-5" onSubmit={handleSubmit(onFormSubmit)}>
+                <input type="text" className="form-control mb-3"  {...register("username")} placeholder="Username" />
+                <input type="password" className="form-control mb-3"  {...register("password")} placeholder="Password" />
+                <input type="email" className="form-control mb-3"  {...register("email")} placeholder="E-mail" />
+                <input type="date" className="form-control mb-3"  {...register("dob")} placeholder="Date of birth" />
+                <input type="file" name="photo" className="form-control mb-3" onChange={(e) => { onFileSelect(e) }} />
 
-        <form className="w-50 mx-auto mt-5" onSubmit={handleSubmit(onFormSubmit)}>
-            <input type="text" className="form-control mb-3"  {...register("username")} placeholder="Username" />
-            <input type="password" className="form-control mb-3"  {...register("password")} placeholder="Password" />
-            <input type="email" className="form-control mb-3"  {...register("email")} placeholder="E-mail" />
-            <input type="date" className="form-control mb-3"  {...register("dob")} placeholder="Date of birth" />
-            <input type="file" name="photo" className="form-control mb-3" onChange={(e) => { onFileSelect(e) }} />
-
-            <button className="btn btn-success">Register</button>
-        </form>
+                <button className="btn btn-success">Register</button>
+            </form>
+        </div>
 
 
     )
